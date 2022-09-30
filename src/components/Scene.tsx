7cb8c6e79e87320e2b394191be8aac1e555eaa46
dimitrykatana <dimitrykatana@gmail.com>
2022-09-30 13:38:08 +0200

    drag FONCTIONNE!!!!!!!

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,31 +1,79 @@
 import { useState } from "react";
 import Pikachu from "./pikachu";
-
 let holding = false
 
-while (holding) {
-  console.log('mouseUp ok')
-}
 
-AFRAME.registerComponent("cursor-listener", {
-  init: function () {
-    let position = this.el.getAttribute("position")
-    this.el.addEventListener("mousedown", function (evt) {
-      holding = true     
-      console.log("mouseUp", holding)
-      // this.setAttribute("position", {
-      //   x: evt.detail.intersection.point.x,
-      //   y: evt.detail.intersection.point.y,
-      //   z: position.z
-      // });
-      // console.log("I was clicked at: ", evt.detail.intersection.point);
-    });
-    
-    this.el.addEventListener("mouseup", function (evt) {
-      holding = false   
-      console.log("mouseUp", holding)
+AFRAME.registerSystem("track-cursor", {
+  init: function() {
+    this.el.setAttribute("cursor", { rayOrigin: "mouse" });
+  }
+});
+
+AFRAME.registerComponent("track-cursor", {
+  init: function() {
+    this.el.addEventListener("mousedown", e => {
+      if (this.el.is("cursor-hovered")) {
+        // this.el.sceneEl.camera.el.setAttribute("look-controls", {
+        //   enabled: false
+        // });
+        this.el.addState("dragging");
+      }
+    })
+    this.el.addEventListener("mouseup", e => {
+      if (this.el.is("dragging")) {
+        // this.el.sceneEl.camera.el.setAttribute("look-controls", {
+        //   enabled: true
+        // });
+        this.el.removeState("dragging");
+      }
     })
   },
+});
+
+AFRAME.registerComponent("dragndrop", {
+  dependencies: ["track-cursor"],
+  init: function() {
+    this.range = 0;
+    this.dist = 0;
+
+    this.el.addEventListener("stateadded", e => {
+      if (e.detail == "dragging") {
+        this.range = 0;
+        this.dist = this.el.object3D.position
+          .clone()
+          .sub(this.el.sceneEl.camera.el.object3D.position)
+          .length();
+      }
+    })
+
+    this.direction = new AFRAME.THREE.Vector3();
+    this.target = new AFRAME.THREE.Vector3();
+    document.addEventListener("wheel", e => {
+      if (e.deltaY < 0) {
+        this.range += 0.1;
+      } else {
+        this.range -= 0.1;
+      }
+    });
+  },
+  updateDirection: function() {
+    this.direction.copy(this.el.sceneEl.getAttribute("raycaster").direction);
+  },
+  updateTarget: function() {
+    let camera = this.el.sceneEl.camera.el
+    this.target.copy(
+      camera.object3D.position
+        .clone()
+        .add(this.direction.clone().multiplyScalar(this.dist + this.range))
+    );
+  },
+  tick: function() {
+    if (this.el.is("dragging")) {
+      this.updateDirection();
+      this.updateTarget();
+      this.el.object3D.position.copy(this.target);
+    }
+  }
 });
 
 const Scene = () => {
@@ -46,8 +94,9 @@ const Scene = () => {
       ></a-circle>
 
       <a-camera class="camera" look-controls position="0 1.5 0">
-        <a-entity
-          cursor="fuse: true; fuseTimeout: 500; rayOrigin: mouse;"
+        <a-entity        
+        cursor="fuse: true; fuseTimeout: 500;"
+        
           position="0 0 -1"
           geometry="primitive: ring; radiusInner: 0.02; radiusOuter: 0.03"
           material="color: black; shader: flat"
@@ -56,7 +105,7 @@ const Scene = () => {
 
       <a-entity
         id="box"
-        cursor-listener
+        dragndrop
         geometry="primitive: box"
         material="color: blue"
         position="2 1 -4"
