e97f457829dad219cbe69d4a628b2d04a24262e8
Quentin Devillers <devi.quentin@gmail.com>
2022-10-10 16:18:41 +0200

    test du drag d'un item depuis le menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,20 +5,30 @@ import { useGLTF} from "@react-three/drei";
 import { useFrame, useThree} from "@react-three/fiber";
 
 export default function menu() {
+    const [nbItem, setNbItem] = useState([{name:"chapeau"}, {name:"bottes"}])
+
+    
+    const test = (e:any, name:String) => {
+      e.dataTransfer.setData('text/plain', name)
+      console.log(e.dataTransfer.getData('text/plain'))
+      console.log("dragStart")
+    } 
+    
+    const Items = ():any => {
+      const items = nbItem.map((a, i) => <li key={i} className="menu-item" data-name={a.name} draggable onDragStart={(e) => test(e, a.name)}></li>)
+      // @ts-nocheck
+      items.forEach(i => i)
+
+      return items
+    }
+
   
 
     return (
       <>
         <div className="menu">
             <ul className="menu-list">
-                <li className="menu-item" draggable></li>
-                <li className="menu-item" draggable></li>
-                <li className="menu-item" draggable></li>
-                <li className="menu-item" draggable></li>
-                <li className="menu-item" draggable></li>
-                <li className="menu-item" draggable></li>
-                <li className="menu-item" draggable></li>
-                <li className="menu-item" draggable></li>
+                <Items />
             </ul>
         </div>
       </>
--- src/types/App.tsx
+++ src/types/App.tsx
@@ -1,20 +1,25 @@
 import * as THREE from "three";
-import { useRef, useState} from "react";
-import { Canvas, useFrame, useLoader} from "@react-three/fiber";
-import { useGLTF, OrbitControls, PointerLockControls, FlyControls} from "@react-three/drei";
+import { useEffect, useRef, useState } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import {
+  useGLTF,
+  OrbitControls,
+  PointerLockControls,
+  FlyControls,
+} from "@react-three/drei";
 import { TextureLoader } from "three";
 import DragDrop from "../components/DragDrop";
 import Menu from "../components/Menu";
 
-const sol = "/assets/images/grass.png"
+const sol = "/assets/images/grass.png";
 
 function Taupiqueur() {
   let mixer: any = null;
   const ref = useRef<THREE.Mesh>(null!);
   const texture = useLoader(TextureLoader, sol);
-  const day = useLoader(TextureLoader,'/assets/images/sky.png');
-  const night = useLoader(TextureLoader,'/assets/images/sky_2.png');
-  
+  const day = useLoader(TextureLoader, "/assets/images/sky.png");
+  const night = useLoader(TextureLoader, "/assets/images/sky_2.png");
+
   const gltf = useGLTF("/assets/images/diglett_pokemon.glb");
   mixer = new THREE.AnimationMixer(gltf.scene);
   mixer.clipAction(gltf.animations[0]).play();
@@ -26,47 +31,82 @@ function Taupiqueur() {
 
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
-    useFrame((state, delta) => (ref.current.rotation.y += 0.007));
-    // tiling de la texture
-      texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-      texture.repeat.set(50, 50);
-      texture.anisotropy = 16;
+  useFrame((state, delta) => (ref.current.rotation.y += 0.007));
+  // tiling de la texture
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set(50, 50);
+  texture.anisotropy = 16;
 
   return (
     <>
-    <mesh  ref={ref} scale={100} position={[0, 0, 0]} onClick={() => click(!clicked)}>
-      <meshStandardMaterial map={clicked ? day : night} side={THREE.DoubleSide}/>
-      <sphereGeometry/>
-    </mesh>
+      <mesh
+        ref={ref}
+        scale={100}
+        position={[0, 0, 0]}
+        onClick={() => click(!clicked)}
+      >
+        <meshStandardMaterial
+          map={clicked ? day : night}
+          side={THREE.DoubleSide}
+        />
+        <sphereGeometry />
+      </mesh>
 
-    <mesh scale={clicked ? 10 : 5} >
+      <mesh scale={clicked ? 10 : 5}>
         {/* <primitive position= {[0, 1, 0]} object= {clicked ? gltf2.scene: gltf.scene} /> */}
-      <DragDrop />
+        <DragDrop />
+      </mesh>
 
-    </mesh>
-
-    <mesh onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} rotation={[-Math.PI / 2, 0, 0]} >
-        <meshStandardMaterial roughness={3} map={texture} 
-        color={clicked ? "limegreen" : "darkgreen"}>
-        </meshStandardMaterial>
-        <circleGeometry args={[100, 100]}/>
-    </mesh>
+      <mesh
+        onPointerOver={() => hover(true)}
+        onPointerOut={() => hover(false)}
+        rotation={[-Math.PI / 2, 0, 0]}
+      >
+        <meshStandardMaterial
+          roughness={3}
+          map={texture}
+          color={clicked ? "limegreen" : "darkgreen"}
+        ></meshStandardMaterial>
+        <circleGeometry args={[100, 100]} />
+      </mesh>
     </>
   );
 }
 
 export default function App() {
+  // const canvas = document.getElementById("canvas-container");
+  const canvas = useRef()
+  
+  useEffect(() => {
+    // @ts-nocheck
+    canvas.current.addEventListener("dragenter", (event: any) => {
+      console.log("Drag détecté dans le canvas");
+      console.log(event);
+      console.log(event.fromElement);
+      // console.log("canvas: ", canvas);
+    });
+  }, []);
+
+
+
+
+
+  // FAIRE UNE VARIABLE GLOBALE DANS LEQUEL ON STOCK LE NOM DE L OBJET DRAGGE
+
+
+
+
+
   return (
     <>
-    <Menu/>
-    <div id="canvas-container">
-      <Canvas camera={{ fov:90, position: [0, 4, 60] }}>
-
-      {/* <FlyControls position={[0, 20, 1]} /> */}
-        <ambientLight position={[0, 5, 6]} intensity={2}/>
-        <Taupiqueur></Taupiqueur>
-      </Canvas>
-    </div>
+      <Menu />
+      <div>
+        <Canvas ref={canvas} id="canvas-container" camera={{ fov: 90, position: [0, 4, 60] }}>
+          <OrbitControls />
+          <ambientLight position={[0, 5, 6]} intensity={2} />
+          <Taupiqueur></Taupiqueur>
+        </Canvas>
+      </div>
     </>
   );
 }
